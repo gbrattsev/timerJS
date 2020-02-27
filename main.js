2dfd6b7be3510025642874b613853a5f8ee3b7bd
Gennady Brattsev <56601654+gbrattsev@users.noreply.github.com>
2020-02-27 09:35:55 +0500

    add local time

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
-  let deadline = '2020-02-29';
+  let deadline = '2020-02-29 GMT+0500';
 
   function getTimeRemaining(endtime) {
     let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t / 1000) % 60),
         minutes = Math.floor((t / 1000 / 60) % 60),
-        hours = Math.floor(t / 1000 / 60 / 60),
+        hours = Math.floor((t / 1000 / 60 / 60) % 24),
         days = Math.floor(t / 1000 / 60 / 60 / 24),
         dayTitle = document.querySelector('.day-title');
 
